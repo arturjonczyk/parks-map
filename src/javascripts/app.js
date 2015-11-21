ec48f56b81b8c12675bb9017a0e558c5d772c2dd
Artur Jonczyk <artur.jonczyk@gmail.com>
2015-11-21 18:52:58 +0100

    basic change

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,7 +1,15 @@
-var $search = $('.filter'),
-	$button = $("#header__button");
-
-$button.on('click', function() {
-  $(this).toggleClass("hamburger--open");
-  $search.toggleClass('filter-open');
+$(document).ready(function() {
+  $("[data-toggle]").click(function() {
+    var toggle_el = $(this).data("toggle");
+    $(toggle_el).toggleClass("open-sidebar");
+  });
+     
 });
+
+// var $search = $('.filter'),
+// 	$button = $("#header__button");
+
+// $button.on('click', function() {
+//   $(this).toggleClass("hamburger--open");
+//   $search.toggleClass('filter-open');
+// });
