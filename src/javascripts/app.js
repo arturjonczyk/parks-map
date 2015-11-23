b134638fc0e7892f28611e99fdc2cf9a1b488180
Artur Jonczyk <artur.jonczyk@gmail.com>
2015-11-23 11:49:54 +0100

    Added map

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -3,4 +3,22 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 		$('.content').toggleClass('is-open');
 	});
+
+	var map;
+	var city = {lat: 52.2187648, lng: 21.0354383};
+
+	function initMap() {
+	    var mapId = document.getElementById('map');
+	    var mapOptions = {
+	        center: city,
+	        zoom: 12,
+
+	        //Limit min/max zoom
+	        minZoom: 2,
+	        maxZoom: 18,
+		};
+		map = new google.maps.Map(mapId, mapOptions);
+	};
+
+	initMap();
 });
