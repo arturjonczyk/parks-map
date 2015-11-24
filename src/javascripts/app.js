3c90c2e21ac027dd2da1982be8006c2429f0a1af
Artur Jonczyk <artur.jonczyk@gmail.com>
2015-11-24 18:08:33 +0100

    Small changes to the previous commit

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,16 +1,28 @@
-$(document).ready(function(){
-	$('#content__hamburger-button').on('click', function(){
-		$(this).toggleClass('open');
+var GreenAreas = (function () {
+	/* add members here */
+
+	var openCloseSidebar = function (self) {
+		$(self).toggleClass('open');
 		$('.content').toggleClass('is-open');
-	});
+	};
 
+	/********* HAMBURGER **********/
+	var $hamburgerButton = $('#content__hamburger-button');
+
+	$hamburgerButton.on('click', function (event) {
+		var self = this;
+		openCloseSidebar(self);
+	})
+	/********* END HAMBURGER **********/
+
+	/********* MAP **********/
 	var map;
-	var city = {lat: 52.2187648, lng: 21.0354383};
+	var cityWarsaw = {lat: 52.2187648, lng: 21.0354383};
 
-	function initMap() {
+	var initMap = function() {
 	    var mapId = document.getElementById('map');
 	    var mapOptions = {
-	        center: city,
+	        center: cityWarsaw,
 	        zoom: 12,
 
 	        //Limit min/max zoom
@@ -20,5 +32,23 @@ $(document).ready(function(){
 		map = new google.maps.Map(mapId, mapOptions);
 	};
 
-	initMap();
-});
+	/********* END MAP **********/
+
+	/********* INITIATION APP COMPONENTS **********/
+	var init = function () {
+		// initiation view-model
+		ko.applyBindings(GreenAreas);
+		// initiation the map
+		initMap();
+	};
+	/* execute the init function when the DOM is ready */
+	$(init);
+	/********* END INITIATION APP COMPONENTS **********/
+
+	/********* PUBLIC FUNCTIONS **********/
+	return {
+		
+	};
+	/********* END PUBLIC FUNCTIONS **********/
+
+})();
