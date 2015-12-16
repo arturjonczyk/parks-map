bfb5de0766aa603df8152d81af164a9e21c004ab
Artur Jonczyk <artur.jonczyk@gmail.com>
2015-12-16 12:15:38 +0100

    Change names of modules

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,38 +1,4 @@
 (function() {
-
-	// var Park = function(name, location, image, rating, url, ratingImg) {
-	// 	this.name = name;
-	// 	this.location = {lat: location.coordinate.latitude, lng: location.coordinate.longitude};
-	// 	this.image = image;
-	// 	this.rating = rating;
-	// 	this.ratingImg = ratingImg;
-	// 	this.url = url;
-	// 	this.init();
-	// };
-
-	// Park.prototype.addMarkerToMap = function() {
-	// 	marker = new google.maps.Marker({
-	// 		position: this.location,
-	// 		map: map
-	// 	});
-	// };
-
-	// Park.prototype.addListenerToMarker = function() {
-	// 	marker.addListener('click', function() {
-	// 		infowindow.open(map, marker);
-	// 	});
-	// };
-
-	// Park.prototype.init = function() {
-	// 	this.addMarkerToMap();
-	// 	this.addListenerToMarker();
-	// };
-
-
-	
-
-
-
 	// *** Hamburger Button - open|close ***
 	// helper for event handler
 	var openCloseSidebar = function (self) {
@@ -53,9 +19,9 @@
 		// initMap();
 		var LOCATION = {lat: 52.2187648, lng: 21.0354383};
 		MapHandler.init(LOCATION);
-		YelpHandler.searchRequest('Warsaw, Poland', 'parks', 'parks');
+		// YelpHandler.searchRequest('Warsaw, Poland', 'parks', 'parks');
 		// init ViewModel
-		ko.applyBindings(ViewModel);
+		ko.applyBindings(ParkViewModel);
 	};
 	// start the app
 	$(init);
