43bf934ccf281c96258334859569fd2a582523e4
Artur Jonczyk <artur.jonczyk@gmail.com>
2015-12-30 18:43:50 +0100

    Added comments to the data.js

--- src/javascripts/Data.js
+++ src/javascripts/Data.js
@@ -1,3 +1,15 @@
+/**
+ * Data for Park app
+ * @type {Object}
+ * parks (array of Park constructors)
+ * @param  {string} name      Name of the park
+ * @param  {float} lat       Location latitude
+ * @param  {float} lng       Location longitude
+ * @param  {string} image     Link of the main park image
+ * @param  {string} ratingImg Link of the rating image
+ * @param  {string} url       Link to the yelp side of the park
+ * markers (array of markers that will be added)
+ */
 var data = {
 	parks: [
 		new Park('Park Skaryszewski', 52.2412561496634, 21.0544395446777, 'http://s3-media2.fl.yelpcdn.com/bphoto/wcwR-OnPfSyLYIoRSP7Mqg/ms.jpg', 'http://s3-media1.fl.yelpcdn.com/assets/2/www/img/f1def11e4e79/ico/stars/v1/stars_5.png', 'http://www.yelp.com/biz/park-skaryszewski-warszawa?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=CtLhQCQjIEPDX_lI1Hdb6'),
